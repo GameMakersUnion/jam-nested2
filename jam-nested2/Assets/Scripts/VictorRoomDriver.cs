e6932971e865b96b56669e948457ed712fa4c884
Victor <victor.nguyen@hotmail.com>
2015-04-04 19:04:50 -0400

    Added my own driver, and some fixes maybe?

---
+++ jam-nested2/Assets/Scripts/VictorRoomDriver.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VictorRoomDriver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		//Room room = new Room();
+		int width = 20;
+		int height = 20; 
+		Vector2 posi = new Vector2(0,0);
+		GameObject go = (GameObject)Instantiate( Resources.Load("tiles/room"), new Vector3(0, 0, 0), Quaternion.identity);
+		Room room = go.GetComponent<Room>();
+		room.Generate(Manager.tile.wall, width, height, posi );
+		room.Draw();
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
